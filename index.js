d06217eaf84490d640be7c4f77cd7556c0fb293c
Mudit <magnifico@protonmail.com>
2018-01-06 20:37:15 +0530

    Socket Implemented

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ var app         = require('express')();
 var config      = require('config');
 var http        = require('http').Server(app);
 var io          = require('socket.io')(http);
+var http        = require('http');
+const url = require('url');
+const WebSocket = require('ws');
 
 require('./routes')(app);
+var comm="";
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ port: 3001 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+wss.on('connection', function connection(ws, req) {
+
+    console.log("Someone Connected");
+    ws.send(comm);
+    
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        comm=message;
+        ws.send(comm);
     });
-    socket.on('message', function(msg){
-      io.emit('message', msg);
-      console.log("Key pressed");
+
+    ws.on('close', function(c, d){
+        console.log('Someone got out');
     });
-  });
-  
-  app.listen(config.get('port'), function(){
-      console.log("Server Running at "+ config.get('port'));
-  });
+});
+
+
+
+app.listen(config.get('port'), function(){
+    console.log("Server Running at "+ config.get('port'));
+});
