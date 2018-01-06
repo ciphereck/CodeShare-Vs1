ec7c6ca29c76bd863f6eb92790443d5894088ea5
ciphereck <magnifico@protonmail.com>
2018-01-06 18:00:10 +0530

    Socket Technology Introduced

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var app         = require('express')();
 var config      = require('config');
-
-app.listen(config.get('port'));
+var http        = require('http').Server(app);
+var io          = require('socket.io')(http);
 
 require('./routes')(app);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('message', function(msg){
+      io.emit('message', msg);
+      console.log("Key pressed");
+    });
+  });
+  
+  app.listen(config.get('port'), function(){
+      console.log("Server Running at "+ config.get('port'));
+  });
